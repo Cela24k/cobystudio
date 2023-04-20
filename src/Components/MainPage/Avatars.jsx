2eb304a2fcae34713997cf34ef0e0df014d49252
Cela24k <882778@stud.unive.it>
2023-04-20 14:25:35 +0200

    made avatar section responsive with bootstrap

--- src/Components/MainPage/Avatars.jsx
+++ src/Components/MainPage/Avatars.jsx
@@ -7,13 +7,13 @@ const evaVideo = (
         playing={true}
         loop={true}
         muted={true}
-        width={'50%'}
+        width={'100%'}
         height={'100%'}
         style={{
-            // position:'absolute',
+            position:'relative',
             zIndex: 1,
-            right:0,
-            bottom:0
+            right: 0,
+            bottom: 0
         }}
     />
 )
@@ -24,34 +24,35 @@ const otherVideo = (
         playing={true}
         loop={true}
         muted={true}
-        width={'50%'}
-        height={'80%'}
+        width={'100%'}
+        height={'100%'}
         style={{
-            position:'absolute',
+            position: 'relative',
             zIndex: 1,
-            right:0,
-            bottom:0
+            right: 0,
+            bottom: 0
         }}
     />
 )
 
 function Avatars() {
 
-    const [avatar,setAvatar] = useState(true);
+    const [avatar, setAvatar] = useState(true);
 
-    function handleChange(event){
+    function handleChange(event) {
         console.log(event);
-        setAvatar((prev)=>{
+        setAvatar((prev) => {
             return !prev;
         })
     }
 
     return (
-        <div className="main b" id="2">
+        
+        <div className="main b container-fluid" id="2">
             <div className="overlay">
             </div>
-            <div className="avatars-content">
-                <div className="avatars-header-wrapper a ">
+            <div className="row avatars-content">
+                <div className="avatars-header-wrapper a col-12 col-sm-6 col-lg-4">
                     <div>
                         <h2 className="my-header">
                             DIGITAL AVATARS
@@ -67,10 +68,42 @@ function Avatars() {
                         </u>
                     </div>
                 </div>
-                {avatar ? evaVideo : otherVideo}
+                <div className="col-12 col-sm-6 col-lg-4 offset-lg-3">
+                    {avatar ? evaVideo : otherVideo}
+                </div>
             </div>
         </div>
     )
 }
+
+// function old() {
+//     return (
+//         <div className="container-fluid">
+//             <div className="row justify-content-end">
+//                 <div className="col-12 col-sm-6 col-lg-4">
+//                     <div className="avatars-header-wrapper a ">
+//                         <div>
+//                             <h2 className="my-header">
+//                                 DIGITAL AVATARS
+//                             </h2>
+//                             <span className="my-span">
+//                                 At Cobystudio we specialize in high quality 3D avatars that can serve as influencers,
+//                                 advertisement models, visual entertainers in the fashion and social media marketing
+//                                 industry. Our avatars are design to perfectly represent your brand or product.
+//                             </span>
+//                             <br />
+//                             <u className="avatar-changer" onClick={handleChange}>
+//                                 Click to switch avatars
+//                             </u>
+//                         </div>
+//                     </div>
+//                 </div>
+//                 <div className="col-12 col-sm-6 col-lg-4">
+//                     {avatar ? evaVideo : otherVideo}
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
 
 export default Avatars;
