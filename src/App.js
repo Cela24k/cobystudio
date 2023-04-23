f844e0c9579aa87ce06f1b68a1873c40b28e5c77
Cela24k <882778@stud.unive.it>
2023-04-24 00:08:21 +0200

    Improved loading (?)
    Now the loading gif is also shown while loading the other 2 models, but the code is less extensible and there is a bit of copy paste so IDK

--- src/App.js
+++ src/App.js
@@ -2,8 +2,27 @@ import './App.scss';
 import DrawerAppBar from './Components/DrawerAppBar';
 import { LinearProgress } from '@mui/material';
 import ColorRadioButtons from './Components/ColorRadiobuttons';
+import { useEffect } from 'react';
+
+const models = [
+  'Cafe.glb',
+  'Cuffie.glb',
+];
 
 function App() {
+
+  useEffect(() => {
+    async function preloadModels() {
+      const promises = models.map(async (model) => {
+        const response = await fetch(model);
+        const buffer = await response.arrayBuffer();
+        const url = URL.createObjectURL(new Blob([buffer]));
+        return url;
+      });
+      await Promise.all(promises);
+    }
+    preloadModels();
+  }, [])
 
   return (
     <div className="App">
--- src/Components/MainPage/Products.jsx
+++ src/Components/MainPage/Products.jsx
@@ -10,38 +10,76 @@ function Products() {
         setProduct(newUrl);
     }
 
-    return (<div className="main c" id="3">
-        <div className="product-scene-wrapper">
-            <model-viewer class="viewer"
-                id="my-model"
-                alt="COBYSTUDIO Bomber"
-                src={product}
-                width="100vw"
-                shadow-intensity="1"
-                camera-controls touch-action="pan-y" autoplay animation-name disable-zoom disable-tap disable-pan
-                poster='Spinner-1s-200px.svg'>
-            </model-viewer>
-            <div className="product-container">
-                <div className="product-list">
-                    <ProductTabs tabEvent={handleTabs} />
+    return (<div className="main c container-fluid" id="3">
+        <div className="row ">
+            <div className="product-scene-wrapper col-12 col-sm-6 col-lg-4">
+                {
+                    product === 'models/gltf/Bomber.glb' ?
+                        <model-viewer class="viewer"
+                            preload
+                            id="my-model"
+                            alt="COBYSTUDIO Bomber"
+                            src='models/gltf/Bomber.glb'
+                            width="100vw"
+                            shadow-intensity="1"
+                            camera-controls touch-action="pan-y" autoplay animation-name disable-zoom disable-tap disable-pan
+                            poster='Spinner-1s-200px.svg'>
+                        </model-viewer>
+                        : null
+                }
+                {
+                    product === 'models/gltf/Cafe.glb' ?
+                        <model-viewer class="viewer"
+                            preload
+                            id="my-model"
+                            alt="COBYSTUDIO Bomber"
+                            src='models/gltf/Cafe.glb'
+                            width="100vw"
+                            shadow-intensity="1"
+                            camera-controls touch-action="pan-y" autoplay animation-name disable-zoom disable-tap disable-pan
+                            poster='Spinner-1s-200px.svg'>
+                        </model-viewer>
+                        :
+                        null
+                }
+                {
+                    product === 'models/gltf/Cuffie.glb' ?
+                        <model-viewer class="viewer"
+                            preload
+                            id="my-model"
+                            alt="COBYSTUDIO Bomber"
+                            src='models/gltf/Cuffie.glb'
+                            width="100vw"
+                            shadow-intensity="1"
+                            camera-controls touch-action="pan-y" autoplay animation-name disable-zoom disable-tap disable-pan
+                            poster='Spinner-1s-200px.svg'>
+                        </model-viewer>
+                        :
+                        null
+                }
+
+                <div className="product-container">
+                    <div className="product-list">
+                        <ProductTabs tabEvent={handleTabs} />
+                    </div>
                 </div>
             </div>
+            {<div className="avatars-header-wrapper b col-12 col-sm-6 col-lg-4">
+                <h2 className="my-header">
+                    PRODUCT PRESENTATION
+                </h2>
+                <span className="my-span">
+                    We understand the importance of showcasing your products.
+                    This is why we offer professional product presentation services that
+                    are designed to help you stand out in the competitive marketplace.
+                    Whether you're looking to create visuals for a website, brochure, trade show,
+                    or even your own virtual gallery, we've got you covered.
+                    With our product presentation services, you can be sure that your that your
+                    products will be presented in the best possible way, making a lasting
+                    impression on your audience.
+                </span>
+            </div>}
         </div>
-        {<div className="avatars-header-wrapper b">
-            <h2 className="my-header">
-                PRODUCT PRESENTATION
-            </h2>
-            <span className="my-span">
-                We understand the importance of showcasing your products.
-                This is why we offer professional product presentation services that
-                are designed to help you stand out in the competitive marketplace.
-                Whether you're looking to create visuals for a website, brochure, trade show,
-                or even your own virtual gallery, we've got you covered.
-                With our product presentation services, you can be sure that your that your
-                products will be presented in the best possible way, making a lasting
-                impression on your audience.
-            </span>
-        </div>}
 
     </div>
     )
