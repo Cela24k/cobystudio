46b582a554fa2c1f5dbb175cab4dfe72f939fd8d
Cela24k <882778@stud.unive.it>
2023-04-24 23:34:42 +0200

    Added contact section's texts

--- src/Components/MainPage/Contact.jsx
+++ src/Components/MainPage/Contact.jsx
@@ -4,8 +4,15 @@ function Contact() {
     return (
         <div className="main e" id="5">
             <div className="image-wrapper">
+                <div className="image-filter-panel"></div>
                 <div className="portfolio-container">
-                    <div className="portfolio-dialog">ciao</div>
+                    <div className="portfolio-dialog">We are proud to showcase our 3D portfolio,
+                        which serves as a powerfull tool for clients looking to Build a
+                        similar 3D portfolio or website. <br></br> Our portfolio is a collection of our best
+                        work showcasing our capabilities and expertise in creating a visually
+                        stunning and highly interactive 3D designs. <br></br> Contact us today to learn
+                        more about how we can help you create a 3D portfolio or website </div>
+                    <div className="portfolio-dialog-link">3D Portfolio</div>
                 </div>
                 <img className="studio-image" src="images/Stanza_Coby.jpg" alt="" />
             </div>
